ff9544149a7faa4c9a18281f6ca068ca3bb82a16
Brian Neaves <brianneaves@Brians-MacBook-Air.local>
2017-08-01 13:10:47 -0400

    Made the results box more accurate. Need to condense all these functions into one printResults function. No reason to have them separate.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
 import { KeyValue } from './keyvalue';
 
-const HEADERS: KeyValue[] = [
-  {key:'hi', value:'there'},
-  {key:'cute', value:'doggy'}
-]
-const QUERYPARAMS: KeyValue[] = [
-  {key:'I am', value:'A query parameter'}
-]
+const HEADERS: KeyValue[] = [{key:'', value:''}]
+const QUERYPARAMS: KeyValue[] = [{key:'', value:''}]
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   headers = HEADERS;
   queryParameters = QUERYPARAMS;
-  authUsername = 'ho!';
+  authUsername = '';
   authPassword  = '';
   dataoptions = ['Plain Text', 'JSON', 'XML'];
   dataOptionSelected = 'JSON';
-  data = 'hum!';
+  data = '';
   httpoptions = ['GET', 'PUT', 'POST', 'DELETE'];
   httpOptionSelected = 'GET';
   url = 'www.example.com';
-  result = 'curl --verbose ' + this.printHeaders(this.headers) + ' ' + this.url + ' ' +this.authUsername + ' ' + this.authPassword + ' ' + this.data + ' ' + this.printQueryParameters(this.queryParameters);
+  result = '';
   addKeyValue(array){
     array.push({key:'', value: ''});
   }
@@ -36,24 +31,44 @@ export class AppComponent {
     array.pop();
   }
   printHeaders(array){
-    var text = '';
+    var text = "";
+    if(array.length == 1 && array[0].key.length == 0 && array[0].value.length == 0){
+      return text; 
+    }
     for(var el in array){
-      text = text + ' --header ' + array[el].key + ':' + array[el].value;
+      text = text + ' --header \"' + array[el].key + '\":\"' + array[el].value + '\" ';
     }
     return text;
   }
   printQueryParameters(array){
     var text = '';
-    for(var el in array){
-      if(el != array[0]){
+    for(var i = 0; i < array.length; i++){
+      if(i > 0){
         text = text + "&";
       }
-      text = text + '?' + array[el].key + '=' + array[el].value;
+      text = text + '?' + array[i].key + '=\"' + array[i].value + '\"';
     }
     return text;
   }
-  makeResult(){
-    this.result = 'curl --verbose ' + this.printHeaders(this.headers) + ' ' + this.url + ' ' +this.authUsername + ' ' + this.authPassword + ' ' + this.data + ' ' + this.printQueryParameters(this.queryParameters);
+  printMethod(){
+    if(this.url.length > 0){         
+      return '--request "' + this.httpOptionSelected + '" ' + this.url;
+    }
   }
-  
+  printAuthentication(){
+    var authentication = "";
+    if(this.authUsername.length > 0 || this.authPassword.length > 0){
+      authentication =  "--user " + "\'" + this.authUsername + ":" + this.authPassword + " ";
+      return authentication;
+    }
+    return authentication;
+  }
+  makeResult(){
+    return this.result = 'curl --verbose ' + this.printHeaders(this.headers) + ' ' + this.printAuthentication() + ' ' + this.printMethod() + ' ' + this.data + ' ' + this.printQueryParameters(this.queryParameters);
+  }
+
+  ngOnInit(){
+    this.makeResult();
+  }
+ 
 }
