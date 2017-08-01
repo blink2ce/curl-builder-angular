22844ddf6ca709c5960133e6323e98b9c4c52812
Brian Neaves <brianneaves@Brians-MacBook-Air.local>
2017-08-01 14:46:48 -0400

    Compressed all of the individual text creation functions into makeResult(). The code is much cleaner now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,45 +30,39 @@ export class AppComponent implements OnInit{
   deleteKeyValue(array){
     array.pop();
   }
-  printHeaders(array){
-    var text = "";
-    if(array.length == 1 && array[0].key.length == 0 && array[0].value.length == 0){
-      return text; 
-    }
-    for(var el in array){
-      text = text + ' --header \"' + array[el].key + '\":\"' + array[el].value + '\" ';
-    }
-    return text;
-  }
-  printQueryParameters(array){
-    var text = '';
-    for(var i = 0; i < array.length; i++){
-      if(i > 0){
-        text = text + "&";
-      }
-      text = text + '?' + array[i].key + '=\"' + array[i].value + '\"';
-    }
-    return text;
-  }
-  printMethod(){
-    if(this.url.length > 0){         
-      return '--request "' + this.httpOptionSelected + '" ' + this.url;
-    }
-  }
-  printAuthentication(){
-    var authentication = "";
-    if(this.authUsername.length > 0 || this.authPassword.length > 0){
-      authentication =  "--user " + "\'" + this.authUsername + ":" + this.authPassword + " ";
-      return authentication;
-    }
-    return authentication;
-  }
-  makeResult(){
-    return this.result = 'curl --verbose ' + this.printHeaders(this.headers) + ' ' + this.printAuthentication() + ' ' + this.printMethod() + ' ' + this.data + ' ' + this.printQueryParameters(this.queryParameters);
-  }
 
   ngOnInit(){
     this.makeResult();
+  }
+  makeResult(){
+    var headersText = "";
+    for(var el in this.headers){
+       if(this.headers[el].key.length > 0 && this.headers[el].value.length > 0){
+        headersText =  + headersText + ' --header \"' + this.headers[el].key + '\":\"' + this.headers[el].value + '\" ';
+      }
+    }
+
+    var QPtext = '';
+    for(var i = 0; i < this.queryParameters.length; i++){
+      if(this.queryParameters[i].key.length > 0 && this.queryParameters[i].value.length > 0){
+        if(i > 0){
+          QPtext = QPtext + "&";
+        }
+        QPtext = QPtext + '?' + this.queryParameters[i].key + '=\"' + this.queryParameters[i].value + '\"';
+      }
+    }
+
+    var methodText = '';
+    if(this.url.length > 0){         
+      methodText = '--request "' + this.httpOptionSelected + '" ' + this.url;
+    }
+
+    var authentication = "";
+    if(this.authUsername.length > 0 || this.authPassword.length > 0){
+      authentication =  "--user " + "\'" + this.authUsername + ":" + this.authPassword + " ";
+    }
+
+    return this.result = 'curl --verbose ' + headersText + ' ' + authentication + ' ' + methodText + ' ' + this.data + ' ' + QPtext;
   }
  
 }
